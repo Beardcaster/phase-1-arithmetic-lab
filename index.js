7a875c2b2c343cddbd525445bd133832eaf9221a
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-01-30 18:04:53 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 14;
+let num4 = 10;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 4, 6, 7, 8, 15, 20);
